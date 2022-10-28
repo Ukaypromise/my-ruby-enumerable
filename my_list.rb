57dfe22bfe4cc2ab6e3786f6fd2d281efbc8268f
Ukaypromise <promiseukaimo@gmail.com>
2022-10-28 10:38:02 -0400

    Test methods

--- my_list.rb
+++ my_list.rb
@@ -11,3 +11,21 @@ class MyList
     @list.each(&block)
   end
 end
+value = MyList.new(1, 2, 3, 4)
+# Test #all?
+value.all? { |e| e < 5 }
+# => true
+value.all? { |e| e > 5 }
+# => false
+
+# Test #any?
+value.any? { |e| e == 2 }
+# => true
+value.any? { |e| e == 5 }
+# => false
+
+# Test #filter
+value.filter(&:even?)
+# => [2, 4]
+
+puts 2+2
